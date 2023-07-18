adee9818de0c15626cde8a2e7e5abc2e0bafe0f4
piercef256 <42844463+piercef256@users.noreply.github.com>
2023-07-17 21:29:03 -0700

    changes

--- app.py
+++ app.py
@@ -6,21 +6,20 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/stock')
+@app.route("/stock")
 def get_stock_data():
-    symbol = request.args.get('symbol')
-    start = request.args.get('start')
-    end = request.args.get('end')
+    symbol = request.args.get("symbol")
+    start = request.args.get("start")
+    end = request.args.get("end")
     if not symbol:
-        return jsonify({'error': 'missing symbol parameter'}), 400
+        return jsonify({"error": "missing symbol parameter"}), 400
 
     stock = yf.Ticker(symbol)
     data = stock.history(start=start, end=end)
 
-    close_prices = {str(k.date()): v for k, v in data['Close'].items()}
-
+    close_prices = {str(k.date()): v for k, v in data["Close"].items()}
     return jsonify(close_prices)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
